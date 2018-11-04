4e36924598dd6a1104b91fc9935f9c364c20ca66
idzharbae <idzharbae@gmail.com>
2018-11-04 10:18:18 +0700

    nambah dikit

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,24 +1,25 @@
 <div id="food-menu" class="light-wrapper">
         <section class="ss-style-top"></section>
         <div class="container inner">
-            <h2 class="section-title text-center">Food Menu</h2>
-            <p class="lead main text-center">There is no sincerer love than the love of food!</p>
+            <h2 class="section-title text-center">Menu</h2>
+            <p class="lead main text-center">Our recommended menu</p>
 
             <div class="row">
                 <div class="swiper-container main-slider" id="myCarousel">
                   <div class="swiper-wrapper">
-                    <div class="swiper-slide slider-bg-position" style="background:url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3gBOGROjPpJTcUthIQBNv88Cn13su6EiXRFiX85W9CYwDnLY3');" data-hash="slide1">
-                        <div class="recom-box">
-                            <h4>Nasi Goreng Telur</h4>
+                    <div class="swiper-slide slider-bg-position" style="background:url('img/nasgor.jpg');" data-hash="slide1">
+                        <div class="recom-box"><br>
+                            <h3>Nasi Goreng Telur</h3>
                             Nasi digoreng pake telor, nikmat.
                         </div>
                     </div>
                     <div class="swiper-slide slider-bg-position" style="background:url('img/pk-banner.jpg')" data-hash="slide2">
                       <div class="center text-center">
-                        <div class="banner">
-                            <h1 class="">FOODPEDIA</h1>
+                        <div class="recom-box"><br>
+                            <h3>FOODPEDIA</h3>
+                            COBA COBA DULU
                         </div>
-                        <div class="subtitle" ><h4>INI HANYALAH COBAAN</h4></div>
+                        
                     </div>
                     </div>
                   </div>
