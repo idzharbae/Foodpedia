23fb5b3b6c721f1e94e1919cbcc74f895c8acf72
Vaddyl <14anonymous03@gmail.com>
2018-11-14 11:03:51 +0700

    faq

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -1,43 +1,77 @@
 <div id="chefs" class="parallax pricing">
-        <div class="container inner">
 
-            <h2 class="section-title text-center">FAQ</h2>
-            <p class="lead main text-center">Frequently Asked Question</p>
 
-            <div class="row text-center chefs">
+  <div class="container inner">
 
-                <div class="col-sm-4">
-                    <div class="col-wrapper">
-                        <div class="icon-wrapper">
-                            <img src="img/chefs/1.jpg">
-                        </div>
-                        <h3>Saransh Goila</h3>
-                        <p>Vivamus sagittis lacuson augue laoreet rutrum faucibus dolor auctor. Cras mattis consectetur purus sit amet fermentum ultricies vehicula.</p>
-                    </div>
-                </div>
+    <h2 class="section-title text-center">FAQ</h2>
+    <p class="lead main text-center">Frequently Asked Question</p>
 
-                <div class="col-sm-4">
-                    <div class="col-wrapper">
-                        <div class="icon-wrapper">
-                            <img src="img/chefs/3.jpg">
-                        </div>
-                        <h3>Jane Doe</h3>
-                        <p>Nullam id dolor id nibh ultricies vehicula ut id elit. Cum sociis natoque penatibus et magnis dis parturient monte nascetur ultricies vehicula. </p>
-                    </div>
-                </div>
+      <div class="panel-group" id="faqAccordion">
+          <div class="panel panel-default ">
+              <div class="panel-heading accordion-toggle question-toggle collapsed" data-toggle="collapse" data-parent="#faqAccordion" data-target="#question0">
+                   <h4 class="panel-title">
+                      <a href="#question0" class="ing">Q: What is Lorem Ipsum?</a>
+                </h4>
 
-                <div class="col-sm-4">
-                    <div class="col-wrapper">
-                        <div class="icon-wrapper">
-                            <img src="{{URL::asset('img/chefs/2.jpg')}}">
-                        </div>
-                        <h3>Anton Mosimann</h3>
-                        <p>Curabitur blandit matti tempus porttitor. Donec id elit non mi porta ut gravida at eget metus. Consectetur adipiscing elit ultricies vehicula.</p>
-                    </div>
+              </div>
+              <div id="question0" class="panel-collapse collapse" style="height: 0px;">
+                  <div class="panel-body">
+                       <h5><span class="label label-primary">Answer</span></h5>
 
-                </div>
-            </div>
+                      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five <a href="http://jquery2dotnet.com/" class="label label-success">http://jquery2dotnet.com/</a> centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                          </p>
+                  </div>
+              </div>
+          </div>
+          <div class="panel panel-default ">
+              <div class="panel-heading accordion-toggle collapsed question-toggle" data-toggle="collapse" data-parent="#faqAccordion" data-target="#question1">
+                   <h4 class="panel-title">
+                      <a href="#question1" class="ing">Q: Why do we use it?</a>
+                </h4>
 
-        </div>
+              </div>
+              <div id="question1" class="panel-collapse collapse" style="height: 0px;">
+                  <div class="panel-body">
+                       <h5><span class="label label-primary">Answer</span></h5>
+
+                      <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+                  </div>
+              </div>
+          </div>
+          <div class="panel panel-default ">
+              <div class="panel-heading accordion-toggle collapsed question-toggle" data-toggle="collapse" data-parent="#faqAccordion" data-target="#question2">
+                   <h4 class="panel-title">
+                      <a href="#question2" class="ing">Q: Where does it come from?</a>
+                </h4>
+
+              </div>
+              <div id="question2" class="panel-collapse collapse" style="height: 0px;">
+                  <div class="panel-body">
+                       <h5><span class="label label-primary">Answer</span></h5>
+
+                      <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+                  </div>
+              </div>
+          </div>
+          <div class="panel panel-default ">
+              <div class="panel-heading accordion-toggle collapsed question-toggle" data-toggle="collapse" data-parent="#faqAccordion" data-target="#question3">
+                   <h4 class="panel-title">
+                      <a href="#question3" class="ing">Q: Where can I get some?</a>
+                </h4>
+
+              </div>
+              <div id="question3" class="panel-collapse collapse" style="height: 0px;">
+                  <div class="panel-body">
+                       <h5><span class="label label-primary">Answer</span></h5>
+
+                      <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. </p>
+                  </div>
+              </div>
+          </div>
+
+      </div>
+      <!--/panel-group-->
+  </div>
+
         <!-- /.container -->
     </div><!-- /#chefs -->
