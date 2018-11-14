d19d54af10a18f6b1e0d90885e9e9e37139b8103
YuukiRen <reinaldoalvin1@gmail.com>
2018-11-14 21:17:07 +0700

    add menu controller

---
+++ app/Http/Controllers/MenuController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Menu;
+
+class MenuController extends Controller
+{
+    public function home(){
+    	$menu = Menu::all();
+    	return view('welcome', ['menu'=>$menu]);
+    }
+
+    public function add(Request $request){
+    	$this->validate($request,[
+    		'name'=>'required',
+            'recommended'=>'required',
+            'categoryId'=>'required'
+    	]);
+    	$menu = new Menu;
+    	$menu->name = $request->input('name');
+        $menu->recommended = $request->input('recommended');
+        $menu->categoryId = $request->input('categoryId');
+    	$menu->save();
+    	return redirect('/')->with('info','Menu Saved Successfully!');
+    }
+    public function update($id){
+    	$menu = Menu::find($id);
+    	return view('update', ['menu'=>$menu]);
+    }
+    public function edit(Request $request, $id){
+    	$this->validate($request,[
+    		'name'=>'required',
+            'recommended'=>'required',
+            'categoryId'=>'required'
+    	]);
+        $menu = Menu::find($id);
+        $menu->name = $request->input('name');
+        $menu->recommended = $request->input('recommended');
+        $menu->categoryId = $request->input('categoryId');
+    	$menu->save();
+        return redirect('/')->with('info','Menu Updated Successfully!');
+    }
+    public function read($id){
+    	$menu=Menu::find($id);
+    	return view('read', ['menu'=>$menu]);
+
+    }
+    public function delete($id){
+    	Menu::where('id',$id)->delete();
+    	return redirect('/')->with('info','Menu Deleted Successfully!');
+    }
+
+}
