abaa8dd60ecf49f940149df0434fbbf231c3c340
idzharbae <idzharbae@gmail.com>
2018-11-14 22:56:29 +0700

    Admin - Message

--- resources/views/admin/faq.blade.php
+++ resources/views/admin/faq.blade.php
@@ -1,1 +1,100 @@
 @extends('layout.adminlayout')
+
+@section('content')
+          <div class="row">
+            <div class="col-md-12">
+              <div class="card">
+                <div class="card-header card-header-primary">
+                  <h4 class="card-title ">Daftar</h4>
+                  <p class="card-category">Daftar FAQ yang sudah dibuat.</p>
+                </div>
+                <div class="card-body">
+                  <div class="table-responsive">
+                    <table class="table">
+                      <thead class=" text-primary">
+                        <th>
+                          Judul
+                        </th>
+                        <th>
+                          Deskripsi
+                        </th>
+                        <th>
+                          Status
+                        </th>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>
+                          	Bagaimana cara makan?
+                          </td>
+                          <td>
+                            Masukan makanan kemulut lalu kunyah dan telan setelah lunak.
+                          </td>
+                          <td>
+                            edit[x]
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            Harga parkir berapa?
+                          </td>
+                          <td>
+                            Seikhlasnya.
+                          </td>
+                          <td>
+                            edit[x]
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            Webnya bagus siapa sih yang buat?
+                          </td>
+                          <td>
+                            Kontak saja idzharbae@gmail.com
+                          </td>
+                          <td>
+                            edit[x]
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-12">
+              <div class="card card-plain">
+                <div class="card-header card-header-primary">
+                  <h4 class="card-title mt-0">Tambah Baru</h4>
+                  <p class="card-category">Tambah FAQ baru ke daftar</p>
+                </div>
+                <div class="card-body">
+                  <div class="table-responsive">
+                    <table class="table table-hover">
+                      <form class="form-horizontal needs-validation" novalidate method="POST"  action="{{ url('/save') }}" enctype="multipart/form-data" >
+                        {{ csrf_field() }}
+                        <div class="row" style="margin-top: 20px;">
+                          <div class="col-md-5">
+                            <div class="form-group">
+                              <label>Judul</label>
+                              <input type="text" class="form-control">
+                            </div>
+                          </div>
+                      </div>
+                      <div class="row" style="margin-top: 20px;">
+                        <div class="col-md-5">
+                            <div class="form-group">
+                              <label>Deskripsi</label>
+                               <input type="text" class="form-control">
+                            </div>
+                          </div>
+                      </div><br><br>
+                      <button type="submit" class="btn btn-primary pull-left">Tambah FAQ</button>
+                      </form>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+@endsection
--- resources/views/admin/message.blade.php
+++ resources/views/admin/message.blade.php
@@ -1,1 +1,99 @@
 @extends('layout.adminlayout')
+
+@section('content')
+          <div class="row">
+            <div class="col-md-12">
+              <div class="card">
+                <div class="card-header card-header-primary">
+                  <h4 class="card-title ">Daftar</h4>
+                  <p class="card-category">Daftar FAQ yang sudah dibuat.</p>
+                </div>
+                <div class="card-body">
+                  <div class="table-responsive">
+                    <table class="table">
+                      <thead class=" text-primary">
+                        <th>
+                          Nama
+                        </th>
+                        <th>
+                          email
+                        </th>
+                        <th>
+                          Subjek
+                        </th>
+                        <th>
+                          Isi Pesan
+                        </th>
+                        <th>
+                          Tanggal
+                        </th>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>
+                          	Krismanti
+                          </td>
+                          <td>
+                            krismanti@pencintamakanan.com
+                          </td>
+                          <td>
+                            Kentang gorengnya terlalu asin
+                          </td>
+                          <td>
+                            Tadi saya makan kentang goreng tapi kayaknya keasinan, tapi tetep enak sih.
+                          </td>
+                          <td>
+                            14 September 2018
+                          </td>
+                          <td>
+                            edit[x]
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            Mr Tajir
+                          </td>
+                          <td>
+                            tajirbanget@yahuu.com
+                          </td>
+                          <td>
+                            Tambah menu baru dong
+                          </td>
+                          <td>
+                            Tadi saya pesen semua menu ternyata uang saya masih sisa.
+                          </td>
+                          <td>
+                            10 September 2018
+                          </td>
+                          <td>
+                            edit[x]
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            Mr Kere
+                          </td>
+                          <td>
+                            kere@kere.com
+                          </td>
+                          <td>
+                            Boleh ngutang?
+                          </td>
+                          <td>
+                            Mau makan di Foodpedia tapi gak punya uang hehe.
+                          </td>
+                          <td>
+                            1 Agustus 2018
+                          </td>
+                          <td>
+                            edit[x]
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+@endsection
