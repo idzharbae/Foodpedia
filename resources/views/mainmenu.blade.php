7e30bb53d7588803db4a7e29ec158d1a230afbc4
idzharbae <idzharbae@gmail.com>
2018-12-11 16:44:52 +0700

    Klik image popup

--- resources/views/mainmenu.blade.php
+++ resources/views/mainmenu.blade.php
@@ -5,15 +5,15 @@
 
             <div class="row">
                 @foreach($menu as $item)
-
+                
                 <div class="col-md-6 col-sm-6">
 
                     <div class="pricing-item">
 
-                        <a href="#"><img class="img-responsive img-thumbnail" src="{{asset($item->image)}}" alt="" style="width: 400px; height:150px;"></a>
+                        <a href="#" data-toggle="modal" data-target="#readArt-{{$item->id}}" ><img class="img-responsive img-thumbnail" src="{{asset($item->image)}}" data-toggle="modal" alt="" style="width: 400px; height:150px;"></a>
 
                         <div class="pricing-item-details">
-                            <h3><a href="#">{{$item->name}}</a></h3>
+                            <h3><a href="#" data-toggle="modal" data-target="#readArt-{{$item->id}}" >{{$item->name}}</a></h3>
                             <p>{{$item->description}}</p>
                             <div style="text-align: center; width: 250px; padding: 10px; border: 5px solid white; margin: 0;">
                                     Rp. {{number_format($item->harga)}}.00-
@@ -25,6 +25,41 @@
                         <div class="clearfix"></div>
                     </div>
                 </div>
+                
+
+                <div id="readArt-{{$item->id}}" class="modal fade" role="dialog" >
+                              <div class="modal-dialog">
+                                <div class="modal-content">
+                                  <div class="modal-header">
+                                    <h5 class="modal-title">{{ $item->name }}</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                      <span aria-hidden="true">&times;</span>
+                                    </button>
+                                  </div>
+                                  <div class="modal-body">
+                                    <div class="row">
+                                      <div class="col-md-8">
+                                          <div>
+                                            <label>{{ $item->name }}</label><br>
+                                            <img class="img-responsive img-cover img-center mb-2" id="preview" src="{{asset($item->image)}}" style="max-height:400px; max-width: 400px;" >
+                                          </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                      <div class="col-md-8">
+                                          <div>
+                                            <p>{{$item->description}}</p>
+                                          </div>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    </div>
+                                  </div>
+                                  
+                                </div>
+                              </div>
+                            </div>
                 @endforeach
             </div>
 
--- resources/views/menu/menu.blade.php
+++ resources/views/menu/menu.blade.php
@@ -5,7 +5,191 @@
  <head>
 
    @include('layout.partials.head')
+<style>
+  
+.modal {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  margin-top: 30px;
+  z-index: 1050;
+  display: none;
+  overflow: hidden;
+  outline: 0;
+}
 
+
+
+.modal-open .modal {
+  overflow-x: hidden;
+  overflow-y: auto;
+}
+
+.modal-dialog {
+  position: relative;
+  width: auto;
+  height: auto;
+  margin: 0.5rem;
+  pointer-events: none;
+}
+
+.modal.fade .modal-dialog {
+  transition: transform 0.3s ease-out;
+  transform: translate(0, -25%);
+}
+
+.modal.show .modal-dialog {
+  transform: translate(0, 0);
+}
+
+.modal-dialog-centered {
+  display: flex;
+  align-items: center;
+  min-height: calc(100% - (0.5rem * 2));
+}
+
+.modal-content {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  pointer-events: auto;
+  background-color: #ffffff;
+  background-clip: padding-box;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  border-radius: 0.3rem;
+  box-shadow: 0 0.25rem 0.5rem rgba(0, 0, 0, 0.5);
+  outline: 0;
+}
+
+.modal-backdrop {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 1040;
+  background-color: #000000;
+}
+
+.modal-backdrop.fade {
+  opacity: 0;
+}
+
+.modal-backdrop.show {
+  opacity: 0.26;
+}
+
+.modal-header {
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  padding: 1rem;
+  border-bottom: 1px solid #e9ecef;
+  border-top-left-radius: 0.3rem;
+  border-top-right-radius: 0.3rem;
+}
+
+.modal-header .close {
+  padding: 1rem;
+  margin: -1rem -1rem -1rem auto;
+}
+
+.modal-title {
+  margin-bottom: 0;
+  line-height: 1.5;
+}
+
+.modal-body {
+  position: relative;
+  flex: 1 1 auto;
+  padding: 1rem;
+}
+
+.modal-body label{
+  margin-top: 20px;
+  font-size: 25px;
+}
+
+.modal-body p{
+  margin-top: 20px;
+  font-size: 18px;
+}
+
+.modal-footer {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  padding: 1rem;
+  border-top: 1px solid #e9ecef;
+}
+
+.modal-footer> :not(:first-child) {
+  margin-left: .25rem;
+}
+
+.modal-footer> :not(:last-child) {
+  margin-right: .25rem;
+}
+
+.modal-scrollbar-measure {
+  position: absolute;
+  top: -9999px;
+  width: 50px;
+  height: 50px;
+  overflow: scroll;
+}
+
+@media (min-width: 576px) {
+  .modal-dialog {
+    max-width: 500px;
+    margin: 1.75rem auto;
+  }
+  .modal-dialog-centered {
+    min-height: calc(100% - (1.75rem * 2));
+  }
+  .modal-content {
+    box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.5);
+  }
+  .modal-sm {
+    max-width: 300px;
+  }
+}
+
+@media (min-width: 992px) {
+  .modal-lg {
+    max-width: 800px;
+  }
+}
+
+.btn-secondary {
+  color: #ffffff;
+  background-color: #6c757d;
+  border-color: #6c757d;
+  box-shadow: none;
+}
+
+.btn-secondary:hover {
+  color: #ffffff;
+  background-color: #5a6268;
+  border-color: #545b62;
+}
+
+.btn-secondary:focus,
+.btn-secondary.focus {
+  box-shadow: none, 0 0 0 0.2rem rgba(108, 117, 125, 0.5);
+}
+
+.btn-secondary.disabled,
+.btn-secondary:disabled {
+  color: #ffffff;
+  background-color: #6c757d;
+  border-color: #6c757d;
+}
+
+</style>
  </head>
 
 
@@ -41,5 +225,10 @@
 
 @include('mainmenu')
 @include('layout.partials.footer')
-
+<script src="js/jquery-2.1.3.min.js"></script>
+    <script src="js/jquery.actual.min.js"></script>
+    <script src="js/jquery.scrollTo.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+    
 </body>
